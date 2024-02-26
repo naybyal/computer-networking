cc9063e896a110670c432cea31f2177855f048e3
naybyal <officiallynabiel@gmail.com>
2024-02-26 21:52:58 +0530

    Unidirectional Chat using UDP

---
+++ unidirectionalChatUsingUDP/client.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+
+#define PORT 33345
+#define SERVER_IP "127.0.0.1"
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sockid;
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_in servaddr;
+
+    sockid = socket(AF_INET, SOCK_DGRAM, 0);
+
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
+    while (1) {
+        printf("Client: ");
+        fgets(buffer, BUFFER_SIZE, stdin);
+        sendto(sockid, (const char *)buffer, strlen(buffer), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+    }
+
+    close(sockid);
+    return 0;
+}
+
---
+++ unidirectionalChatUsingUDP/server.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+
+#define PORT 33345
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sockid;
+    struct sockaddr_in servaddr, cliaddr;
+    char buffer[BUFFER_SIZE];
+
+    sockid = socket(AF_INET, SOCK_DGRAM, 0);
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr, 0, sizeof(cliaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
+
+    bind(sockid, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+
+    int len, n;
+    len = sizeof(cliaddr);
+
+    while (1) {
+        n = recvfrom(sockid, (char *)buffer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *)&cliaddr, &len);
+        buffer[n] = '\0';
+        printf("Client : %s", buffer);
+    }
+
+    return 0;
+}
+
