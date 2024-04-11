93bfeb20cace5f585526442b3dc42f20ccb90d1a
naybyal <officiallynabiel@gmail.com>
2024-04-11 20:10:22 +0530

    Unidirectional Chat using TCP - improved code

--- practice/apr11/uniTCP/client.c
+++ practice/apr11/uniTCP/client.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
+
 #include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 #define BUFFER_SIZE 1024
     /*  CLIENT SIDE */
