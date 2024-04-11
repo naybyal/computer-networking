00a49e00ab546552b09dfcacbf156b1def9df865
naybyal <officiallynabiel@gmail.com>
2024-04-11 20:07:38 +0530

    Determine Host Byte Order

--- determineHostByteOrder/determine.c
+++ determineHostByteOrder/determine.c
@@ -8,6 +8,14 @@ int main(int argc, char **argv) {
         char c[sizeof(short)];
     } un;
 
+    /*
+        The line un.s = 0x0102; sets the value of the short integer s to 0x0102, which is equal to 258 in decimal.
+        If the system is big-endian, the first byte of s will be 0x01 and the second byte will be 0x02. 
+        If the system is little-endian, the first byte of s will be 0x02 and the second byte will be 0x01.
+
+        The reason the decimal value 258 is used in this code is because it is a convenient value that can be used to demonstrate the byte order of a short integer on both big-endian and little-endian systems. 
+        The hexadecimal value 0x0102 is easy to recognize in both byte orders, making it a useful value for this purpose.
+    */
     un.s = 0x0102;
     printf("%s\t->\t", CPU_VENDOR_OS);
 
